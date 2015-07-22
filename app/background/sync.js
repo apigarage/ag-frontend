be70b43d05630c36927f563ae602853b81e19ad6
Chinmay Patel <chinmay@chinmay.ca>
2015-07-22 12:53:16 -0400

    Adding Environment Config. Continuous Syncing. Communication between frontend and backend.

--- app/background/sync.js
+++ app/background/sync.js
@@ -1,12 +1,13 @@
 var conf = require('../vendor/electron_boilerplate/env_config');
 var utils = require('../utils.js');
-
+var ipc = require('ipc');
+var wm = require('../windowsManager.js');
 var running = false;
 
 module.exports = function(){
   console.log('Starting the Sync');
   setInterval(syncAgain, conf.syncTime);
-}
+};
 
 function syncAgain(){
   if(!utils.areWeOnline()){
@@ -15,7 +16,10 @@ function syncAgain(){
   }
   if(!running){
     running = true;
-    console.log('Syncing Again ... ' + (new Date().toJSON()) );
+    var d = new Date().toJSON();
+    wm.sendToAllWindows('last-synced-at', d);
+    console.log('Syncing Again ... ' + d );
+    // ipc.send('last-synced-at', d);
     // ALL THE SYNCING GOES HERE.
     // UPDATE THE VIEW OBJECT.
     running = false;
--- app/main.js
+++ app/main.js
@@ -1,48 +1,21 @@
-'use strict';
+(function(){
+  'use strict';
+  /*global require: false */
+  var app = require('app');
+  // Make sure utils are initialized quite early to set online-offline status.
+  require('./utils.js');
+  var wm = require('./windowsManager.js');
 
-var app = require('app');
-// Make sure utils are initialized quite early to set online-offline status.
-var utils = require('./utils.js');
+  var mainWindow;
 
-var BrowserWindow = require('browser-window');
-var env = require('./vendor/electron_boilerplate/env_config');
-var devHelper = require('./vendor/electron_boilerplate/dev_helper');
-var windowStateKeeper = require('./vendor/electron_boilerplate/window_state');
-var mainWindow;
-
-// Preserver of the window size and position between app launches.
-var mainWindowState = windowStateKeeper('main', {
-  width: 1000,
-  height: 600
-});
-
-app.on('ready', function () {
-
-  mainWindow = new BrowserWindow({
-    x: mainWindowState.x,
-    y: mainWindowState.y,
-    width: mainWindowState.width,
-    height: mainWindowState.height
+  app.on('ready', function () {
+    var mainWindow = wm.createWindow();
+    mainWindow.loadUrl("file://" + __dirname + "/app.html");
   });
 
-  if (mainWindowState.isMaximized) {
-    mainWindow.maximize();
-  }
-
-  mainWindow.loadUrl('file://' + __dirname + '/app.html');
-
-  if (env.name === 'development') {
-    devHelper.setDevMenu();
-    mainWindow.openDevTools();
-  }
-
-  mainWindow.on('close', function () {
-    mainWindowState.saveState(mainWindow);
+  app.on('window-all-closed', function () {
+    app.quit();
   });
-});
 
-app.on('window-all-closed', function () {
-  app.quit();
-});
-
-require('./background/start.js')();
+  require('./background/start.js')();
+})();
