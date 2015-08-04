1530a96fa87bc63cbffc669f53e3560ff21cc06b
Chinmay Patel <chinmay@chinmay.ca>
2015-08-04 13:56:01 -0400

    Using proper config values.

--- app/browser/js/services/Config.js
+++ app/browser/js/services/Config.js
@@ -6,13 +6,6 @@ angular.module('app')
  /*
  * All the application Configs
  */
-.factory('Config', ['$http', function ($http) {
-  return {
-    env: "chrome",
-    url: "http://localhost:3000/",
-    // url: "http://api.apigarage.com/",
-    api: "api/",
-    client_id: "id_z7y3e0902uNtMxO07Z6q",
-    client_secret: "secret_44207Z6q1Lq5e6me0902"
-  };
-}]);
+ .factory('Config', ['$http', '$window', function ($http, $window) {
+   return $window.env;
+ }]);
--- app/vendor/electron_boilerplate/env_config.js
+++ app/vendor/electron_boilerplate/env_config.js
@@ -4,12 +4,22 @@
 (function () {
     'use strict';
     var jetpack = require('fs-jetpack');
+
+    // For testing
+    // Find the current directory from dirname, and compare it with 'tests'.
+    // __dirname returns the full path of the directory.
+    var dirnames = __dirname.split('/');
+    if(dirnames[dirnames.length-1] === 'tests'){
+      window.env = jetpack.read(__dirname + '/../build/env_config.json', 'json');
+      return;
+    }
+
+    // For everything except testing.
     if (typeof window === 'object') {
         // Web browser context, __dirname points to folder where app.html file is.
         window.env = jetpack.read(__dirname + '/../env_config.json', 'json');
     } else {
         // Node context
-
         module.exports = jetpack.read(__dirname + '/../../env_config.json', 'json');
     }
 }());
