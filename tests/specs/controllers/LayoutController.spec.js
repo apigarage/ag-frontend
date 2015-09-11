fcae7e0b21600d5caba6e4fe5354619de9adfebb
Chinmay Patel <chinmay@chinmay.ca>
2015-09-11 16:44:02 -0400

    isFinite to isNaN

--- tests/specs/controllers/LayoutController.spec.js
+++ tests/specs/controllers/LayoutController.spec.js
@@ -23,97 +23,185 @@ describe('Controller: LayoutController', function() {
 
   describe('When LayoutCtrl is loaded with valid currentProjectId', function(){
 
-    beforeEach(function(){
-      $rootScope.currentProjectId = 4;
-      $controller('LayoutCtrl', {
-        $scope: $scope,
-        $rootScope: $rootScope,
-      });
-      var project = ProjectsFixtures.get('projectWithTwoCollectionsAndOneItem');
-      $rootScope.currentProjectId = project.id;
+    describe('When currentProjectId is a number', function(){
+      beforeEach(function(){
+        $rootScope.currentProjectId = '4';
+        $controller('LayoutCtrl', {
+          $scope: $scope,
+          $rootScope: $rootScope,
+        });
+        var project = ProjectsFixtures.get('projectWithTwoCollectionsAndOneItem');
+        $rootScope.currentProjectId = project.id;
 
-      // Stubbing Project CRUD
-      var projectStub = ProjectsFixtures.getStub('retreiveProjectWithTwoCollectionsAndOneItem');
-      HttpBackendBuilder.build(projectStub.request, projectStub.response);
+        // Stubbing Project CRUD
+        var projectStub = ProjectsFixtures.getStub('retreiveProjectWithTwoCollectionsAndOneItem');
+        HttpBackendBuilder.build(projectStub.request, projectStub.response);
 
-      $httpBackend.flush();
-    });
-
-    afterEach(function(){
-      $scope.$digest();
-      $rootScope.$apply();
-    });
-
-    it('If the current project id is valid, load the project', function(){
-      expect($rootScope.currentProject).toBeDefined();
-      expect($rootScope.currentProject.collections).toBeDefined();
-      expect(_.isObject($rootScope.currentProject.collections)).toEqual(true);
-      Object.keys($rootScope.currentProject.collections).forEach(function(key){
-        var collection = $rootScope.currentProject.collections[key];
-        expect(_.isObject(collection.items)).toEqual(true);
-      });
-      expect($rootScope.currentProject.items).toBeDefined();
-      expect(_.isObject($rootScope.currentProject.items)).toEqual(true);
-    });
-
-    it('Sidebar is not expanded', function(){
-      expect($scope.layout.sidebarExpanded).toEqual(false);
-    });
-
-    it('History is not expanded', function(){
-      expect($scope.layout.historyMaximized).toEqual(false);
-    });
-
-    it('Application is online', function(){
-      $scope.setConnectionStatus(true);
-      expect($scope.online).toEqual(true);
-      expect($scope.connectionStatus).toBe("");
-    });
-
-    it('Application is offline', function(){
-      $scope.setConnectionStatus(false);
-      expect($scope.online).toEqual(false);
-      expect($scope.connectionStatus).toBe("Offline");
-    });
-
-    describe('When Sidebar is toggled', function(){
-      it('and when the toggleSidebar value is true, it will be false',function(){
-        $scope.layout.sidebarExpanded = false;
-        $scope.toggleSidebar();
-        expect($scope.layout.sidebarExpanded).toEqual(true);
+        $httpBackend.flush();
       });
 
-      it('and when the toggleSidebar value is false, it will be true',function(){
-        $scope.layout.sidebarExpanded = true;
-        $scope.toggleSidebar();
+      afterEach(function(){
+        $scope.$digest();
+        $rootScope.$apply();
+      });
+
+      it('If the current project id is valid, load the project', function(){
+        expect($rootScope.currentProject).toBeDefined();
+        expect($rootScope.currentProject.collections).toBeDefined();
+        expect(_.isObject($rootScope.currentProject.collections)).toEqual(true);
+        Object.keys($rootScope.currentProject.collections).forEach(function(key){
+          var collection = $rootScope.currentProject.collections[key];
+          expect(_.isObject(collection.items)).toEqual(true);
+        });
+        expect($rootScope.currentProject.items).toBeDefined();
+        expect(_.isObject($rootScope.currentProject.items)).toEqual(true);
+      });
+
+    });
+
+    describe('When currentProjectId is a number', function(){
+      beforeEach(function(){
+        $rootScope.currentProjectId = 4;
+        $controller('LayoutCtrl', {
+          $scope: $scope,
+          $rootScope: $rootScope,
+        });
+        var project = ProjectsFixtures.get('projectWithTwoCollectionsAndOneItem');
+        $rootScope.currentProjectId = project.id;
+
+        // Stubbing Project CRUD
+        var projectStub = ProjectsFixtures.getStub('retreiveProjectWithTwoCollectionsAndOneItem');
+        HttpBackendBuilder.build(projectStub.request, projectStub.response);
+
+        $httpBackend.flush();
+      });
+
+      afterEach(function(){
+        $scope.$digest();
+        $rootScope.$apply();
+      });
+
+      it('If the current project id is valid, load the project', function(){
+        expect($rootScope.currentProject).toBeDefined();
+        expect($rootScope.currentProject.collections).toBeDefined();
+        expect(_.isObject($rootScope.currentProject.collections)).toEqual(true);
+        Object.keys($rootScope.currentProject.collections).forEach(function(key){
+          var collection = $rootScope.currentProject.collections[key];
+          expect(_.isObject(collection.items)).toEqual(true);
+        });
+        expect($rootScope.currentProject.items).toBeDefined();
+        expect(_.isObject($rootScope.currentProject.items)).toEqual(true);
+      });
+
+      it('Sidebar is not expanded', function(){
         expect($scope.layout.sidebarExpanded).toEqual(false);
       });
-    });
 
-    describe('When history is toggled', function(){
-      it('and when the historyMaximized value is true, it will be false',function(){
-        $scope.layout.historyMaximized = false;
-        $scope.toggleHistory();
-        expect($scope.layout.historyMaximized).toEqual(true);
-      });
-
-      it('and when the historyMaximized value is false, it will be true',function(){
-        $scope.layout.historyMaximized = true;
-        $scope.toggleHistory();
+      it('History is not expanded', function(){
         expect($scope.layout.historyMaximized).toEqual(false);
       });
-    });
 
-    describe('When external link is clicked', function(){
-      it('opens the external link',function(){
-        // Not sure, how to test it yet.
-        // So, until we do that, this ia dummy test.
-        $scope.openExternal('http://www.google.com');
-        expect(true).toEqual(true);
+      it('Application is online', function(){
+        $scope.setConnectionStatus(true);
+        expect($scope.online).toEqual(true);
+        expect($scope.connectionStatus).toBe("");
+      });
+
+      it('Application is offline', function(){
+        $scope.setConnectionStatus(false);
+        expect($scope.online).toEqual(false);
+        expect($scope.connectionStatus).toBe("Offline");
+      });
+
+      describe('When Sidebar is toggled', function(){
+        it('and when the toggleSidebar value is true, it will be false',function(){
+          $scope.layout.sidebarExpanded = false;
+          $scope.toggleSidebar();
+          expect($scope.layout.sidebarExpanded).toEqual(true);
+        });
+
+        it('and when the toggleSidebar value is false, it will be true',function(){
+          $scope.layout.sidebarExpanded = true;
+          $scope.toggleSidebar();
+          expect($scope.layout.sidebarExpanded).toEqual(false);
+        });
+      });
+
+      describe('When history is toggled', function(){
+        it('and when the historyMaximized value is true, it will be false',function(){
+          $scope.layout.historyMaximized = false;
+          $scope.toggleHistory();
+          expect($scope.layout.historyMaximized).toEqual(true);
+        });
+
+        it('and when the historyMaximized value is false, it will be true',function(){
+          $scope.layout.historyMaximized = true;
+          $scope.toggleHistory();
+          expect($scope.layout.historyMaximized).toEqual(false);
+        });
+      });
+
+      describe('When external link is clicked', function(){
+        it('opens the external link',function(){
+          // Not sure, how to test it yet.
+          // So, until we do that, this ia dummy test.
+          $scope.openExternal('http://www.google.com');
+          expect(true).toEqual(true);
+        });
       });
     });
 
   });
 
-  xit('If the current project id is invalid, redirect to projects page', function(){});
+  describe('If the current project id is invalid, redirect to projects page', function(){
+
+    describe('When currentProjectId is undefined string', function(){
+
+      beforeEach(function(){
+        $rootScope.currentProjectId = 'undefined';
+        $controller('LayoutCtrl', {
+          $scope: $scope,
+          $rootScope: $rootScope,
+        });
+      });
+
+      it('will not load the project', function(){
+        // if you get upto here, means you are passing the test.
+        expect(true).toBe(true);
+      });
+    });
+
+    describe('When currentProjectId is undefined', function(){
+
+      beforeEach(function(){
+        $rootScope.currentProjectId = undefined;
+        $controller('LayoutCtrl', {
+          $scope: $scope,
+          $rootScope: $rootScope,
+        });
+      });
+
+      it('will not load the project', function(){
+        // if you get upto here, means you are passing the test.
+        expect(true).toBe(true);
+      });
+    });
+
+    describe('When currentProjectId is null', function(){
+
+      beforeEach(function(){
+        $rootScope.currentProjectId = null;
+        $controller('LayoutCtrl', {
+          $scope: $scope,
+          $rootScope: $rootScope,
+        });
+      });
+
+      it('will not load the project', function(){
+        // if you get upto here, means you are passing the test.
+        expect(true).toBe(true);
+      });
+    });
+
+  });
 });
