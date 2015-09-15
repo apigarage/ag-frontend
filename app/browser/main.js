708393db45a82bc211d30f8b878becfaa054debe
Chinmay Patel <chinmay@chinmay.ca>
2015-09-15 10:01:27 -0400

    showing menu only for OSX

--- app/browser/main.js
+++ app/browser/main.js
@@ -5,7 +5,7 @@
   var env = require('../vendor/electron_boilerplate/env_config');
   var Menu = require('menu');
   var MenuItem = require('menu-item');
-
+  var utils = require('../../tasks/utils.js');
 
   module.exports.init = function(){
 
@@ -17,120 +17,119 @@
         'min-height': 640
       });
       mainWindow.loadUrl("file://" + __dirname + "/index.html");
-      
-      var template = [{
-        label: 'API Garage',
-        submenu: [
-          {
-            label: 'Hide API Garage',
-            accelerator: 'Command+H',
-            selector: 'hide:'
-          },
-          {
-            label: 'Hide Others',
-            accelerator: 'Command+Shift+H',
-            selector: 'hideOtherApplications:'
-          },
-          {
-            label: 'Show All',
-            selector: 'unhideAllApplications:'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Quit',
-            accelerator: 'Command+Q',
-            click: function() { app.quit(); }
-          },
-        ]
-      },
-      {
-        label: 'Edit',
-        submenu: [
-          {
-            label: 'Undo',
-            accelerator: 'Command+Z',
-            selector: 'undo:'
-          },
-          {
-            label: 'Redo',
-            accelerator: 'Shift+Command+Z',
-            selector: 'redo:'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Cut',
-            accelerator: 'Command+X',
-            selector: 'cut:'
-          },
-          {
-            label: 'Copy',
-            accelerator: 'Command+C',
-            selector: 'copy:'
-          },
-          {
-            label: 'Paste',
-            accelerator: 'Command+V',
-            selector: 'paste:'
-          },
-          {
-            label: 'Select All',
-            accelerator: 'Command+A',
-            selector: 'selectAll:'
-          },
-        ]
-      },
-      {
-        label: 'Window',
-        submenu: [
-          {
-            label: 'Minimize',
-            accelerator: 'Command+M',
-            selector: 'performMiniaturize:'
-          },
-          {
-            label: 'Close',
-            accelerator: 'Command+W',
-            selector: 'performClose:'
-          },
-          {
-            type: 'separator'
-          },
-          {
-            label: 'Bring All to Front',
-            selector: 'arrangeInFront:'
-          },
-        ]
-      },
-      {
-        label: 'Help',
-        submenu: []
-      }];
 
+      if(utils.os() === 'osx'){
 
-      if (env.name === 'development') {
-        template.push({
-          label: 'View',
+        var template = [{
+          label: 'API Garage',
           submenu: [
             {
-              label: 'Reload',
-              accelerator: 'Command+R',
-              click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+              label: 'Hide API Garage',
+              accelerator: 'Command+H',
+              selector: 'hide:'
             },
             {
-              label: 'Toggle DevTools',
-              accelerator: 'Alt+Command+I',
-              click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+              label: 'Hide Others',
+              accelerator: 'Command+Shift+H',
+              selector: 'hideOtherApplications:'
+            },
+            {
+              label: 'Show All',
+              selector: 'unhideAllApplications:'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Quit',
+              accelerator: 'Command+Q',
+              click: function() { app.quit(); }
             },
           ]
-        });
-      }
+        },
+        {
+          label: 'Edit',
+          submenu: [
+            {
+              label: 'Undo',
+              accelerator: 'CmdOrCtrl+Z',
+              selector: 'undo:'
+            },
+            {
+              label: 'Redo',
+              accelerator: 'Shift+CmdOrCtrl+Z',
+              selector: 'redo:'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Cut',
+              accelerator: 'CmdOrCtrl+X',
+              selector: 'cut:'
+            },
+            {
+              label: 'Copy',
+              accelerator: 'CmdOrCtrl+C',
+              selector: 'copy:'
+            },
+            {
+              label: 'Paste',
+              accelerator: 'CmdOrCtrl+V',
+              selector: 'paste:'
+            },
+            {
+              label: 'Select All',
+              accelerator: 'CmdOrCtrl+A',
+              selector: 'selectAll:'
+            },
+          ]
+        },
+        {
+          label: 'Window',
+          submenu: [
+            {
+              label: 'Minimize',
+              accelerator: 'CmdOrCtrl+M',
+              selector: 'performMiniaturize:'
+            },
+            {
+              label: 'Close',
+              accelerator: 'CmdOrCtrl+W',
+              selector: 'performClose:'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              label: 'Bring All to Front',
+              selector: 'arrangeInFront:'
+            },
+          ]
+        }];
 
-      var menu = Menu.buildFromTemplate(template);
-      Menu.setApplicationMenu(menu);
+
+        if (env.name === 'development') {
+          template.push({
+            label: 'View',
+            submenu: [
+              {
+                label: 'Reload',
+                accelerator: 'CmdOrCtrl+R',
+                click: function() { BrowserWindow.getFocusedWindow().reloadIgnoringCache(); }
+              },
+              {
+                label: 'Toggle DevTools',
+                accelerator: 'Alt+CmdOrCtrl+I',
+                click: function() { BrowserWindow.getFocusedWindow().toggleDevTools(); }
+              },
+            ]
+          });
+        }
+
+        var menu = Menu.buildFromTemplate(template);
+        Menu.setApplicationMenu(menu);
+      }
     });
 
     app.on('window-all-closed', function () {
