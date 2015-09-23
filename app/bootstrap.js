a118c8293db3406cd3917f6041448902746ac651
Chinmay Patel <chinmay@chinmay.ca>
2015-09-23 08:31:53 -0400

    fixing ssl issue.

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -6,6 +6,8 @@
   var app = require('app');
   var args = require('yargs').argv;
 
+  app.commandLine.appendSwitch ('ignore-certificate-errors', 'true');
+
   if(args._.length > 0){
     var cli = require('./cli/main.js');
     cli.init();
