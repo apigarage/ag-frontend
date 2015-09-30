61d0389597c9ce07e128e9f0c72fa0bf54164bf9
Chinmay Patel <chinmay@chinmay.ca>
2015-09-30 16:10:16 -0400

    Looking for an update.

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -5,6 +5,7 @@
   /*global require: false */
   var app = require('app');
   var args = require('yargs').argv;
+  
   var updater = require('./common/helpers/updater.js');
   updater.init();
 
--- app/common/helpers/updater.js
+++ app/common/helpers/updater.js
@@ -1,27 +1,72 @@
 (function(){
   'use strict';
+  var app = require('app');
   var q = require('q');
+  var semver = require('semver');
+
   var config = require('../../vendor/electron_boilerplate/env_config');
+  var request = require('./request');
+
   var running = false;
 
+  var updateUrl = 'http://0.0.0.0:8080/package.json';
+  var updateCheckInterval = 60 * 60 * 1000 ;
+
+
   module.exports.init = function(){
-    setInterval(module.exports.check, 60 * 60 ); // Check for updates every hour.
+    console.log('Initializing Updater');
+    start();
+    setInterval(start, updateCheckInterval);
   };
 
-  module.exports.check = function(){
-    try{
-      if(!running){
-        running = true;
-        var d = new Date().toJSON();
-        console.log('Checking for updates at : ', d);
-      }
+  function start(){
+    if(running){
+      console.log('updater is running already.');
+      return;
     }
-    catch(err){}
-    running = false;
+
+    running = true;
+    var d = new Date().toJSON();
+    console.log('Running updater at : ', d);
+
+    return module.exports.checkForUpdates()
+      .finally(function(){
+        running = false;
+      });
+  }
+
+  module.exports.checkForUpdates = function(){
+      try{
+
+        var options = {
+          url: updateUrl,
+          method: 'get'
+        };
+
+        console.log('Checking for updates online.');
+        return request.send(options).then(function(response){
+          if(response && response.body && response.body.latestVersion ){
+            // console.log('Latest online version is: ', response.body.latestVersion);
+            // console.log('Current app version is: ', app.getVersion());
+            var updateAvailable =  semver.lt(app.getVersion(), response.body.latestVersion);
+            if(updateAvailable){
+              console.log('Update is available.');
+              return downloadUpdate();
+            }
+          }
+          console.log('Already running at the latest version');
+        });
+      }
+      catch(err){
+        console.log('Checking for updates failed...');
+        return q.resolve();
+      }
   };
+
 
   module.exports.downloadUpdate = function(){
     // Download the app.asar file to tmp
+    // TODO - Emit Update Downloaded
   };
 
   module.exports.applyUpdate = function(){
