fe3e129cc1c7debf0ee8269a96dd39dcea54ec72
Zad <zadkiel.m@gmail.com>
2015-10-22 21:20:12 -0400

    - add editor activity properties to directive

--- app/browser/js/controller/EditorActivitiesCtrl.js
+++ app/browser/js/controller/EditorActivitiesCtrl.js
@@ -1,4 +1,4 @@
-angular.module('app').controller('EditorCtrl', [
+angular.module('app').controller('EditorActivitiesCtrl', [
   'lodash',
   '$scope',
   '$rootScope',
@@ -15,10 +15,37 @@ angular.module('app').controller('EditorCtrl', [
   'Collections',
   'Projects',
   'Editor',
+  'Activities',
   function (_, $scope, $rootScope, $window, $filter, $http, $sce, $modal, $q,
-    $timeout, $focus, RequestUtility, History, Collections, Projects, Editor){
+    $timeout, $focus, RequestUtility, History, Collections, Projects, Editor,
+    Activities){
+      console.log($rootScope);
 
       // Pass item uuid from Editor Controller to this controller using Directive.
-      // pass the each item data forward to editor-activity-item
+
+
+      $scope.$watch('parentId',function(){
+        // set to loading
+        // get all Actitivies from the Item
+        console.log('parentId', $scope.parentId);
+        Activities.get($scope.parentId, $rootScope.currentProjectId).then(function(activities){
+          console.log('activities',activities);
+          // pass the each item data forward to editor-activity-item
+          if(activities.status == 500){
+            console.log('Whoops! There was an error.');
+            $scope.showActivity = false;
+          }else{
+
+          //   activities.data = { 'type' : 'resolve',
+          //   'time': Date.now(),
+          //   'user': 'Zad'
+          // };
+          }
+
+          }
+        });
+
+      });
+
 
   }]);
--- app/browser/js/directive/EditorActivitiesDirective.js
+++ app/browser/js/directive/EditorActivitiesDirective.js
@@ -1,12 +1,13 @@
 angular.module('app').
-directive("editorActivities", function() {
+directive("editorActivities", ["Activities", function( activities ) {
   return {
     restrict: 'E',
     templateUrl: 'html/editor-activity.html',
     controller: 'EditorActivitiesCtrl',
     controllerAs: 'EditorActivitiesCtrl',
     scope: {
-
+      parentType : "=",
+      parentId : "="
     }
   };
-});
+}]);
